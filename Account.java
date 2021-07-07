660a3269ba005270f5ea64274ec6da11713263d7
Mesquita <mesquita@aospa.co>
2021-07-07 13:51:13 -0300

    Bank: Move account info to its own class
    Signed-off-by: Mesquita <mesquita@aospa.co>

---
+++ Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    private int mType;
+    private int mNumber;
+
+    Account(int type, int number) {
+        mType = type;
+        mNumber = number;
+    }
+}
--- Bank.java
+++ Bank.java
@@ -2,12 +2,11 @@ public class Bank {
     private String mName;
     private int mNumber;
     private int mAgency;
-    private int mAccount;
+    private Account mAccount;
     
-    Bank(String name, int number, int agency, int account) {
+    Bank(String name, int number, int agency) {
         mName = name;
         mNumber = number;
         mAgency = agency;
-        mAccount = account;
     }
 }
